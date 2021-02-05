12b7e06901577947daa1df439a92e010be019fdf
Lucas Mauro <lucasmaurodesouza@gmail.com>
2021-02-05 19:43:39 -0300

    Remove unrequired omitempty flag

--- src/book/interface.go
+++ src/book/interface.go
@@ -4,5 +4,5 @@ type Book struct {
 	Id     string `json:"id,omitempty"`
 	Name   string `json:"name,omitempty"`
 	Author string `json:"author,omitempty"`
-	Read   bool   `json:"read,omitempty"`
+	Read   bool   `json:"read"`
 }
